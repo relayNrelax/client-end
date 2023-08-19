8226ee96a8201dfa1503b81c535abc23bb60f58e
Biptu <c.biptu.pc.mail@gmail.com>
2023-08-19 08:31:59 -0700

    homepage: completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Home from "./Home/Home";
-import Navbar from "./Navbar/Navbar";
+import React from 'react';
+import GlobalStyle from './global-styles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <GlobalStyle />
       <Navbar/>
-      <Home/>
-    </div>
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/signup' exact element={<Signup/>} />
+        <Route path='/login' exact element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
