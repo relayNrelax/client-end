1c449d8f2fe52d5fd1bcae036ca37ad931e27070
Bishal Deb <102280193+bishald0013@users.noreply.github.com>
2023-09-17 15:11:33 +0530

    added

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { userAuthApi } from "../services/userAuthApi";
 
-
 export const store = configureStore({
     reducer: {
         [userAuthApi.reducerPath]: userAuthApi.reducer
